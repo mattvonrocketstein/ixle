03fdd7809dd4632a2ba42d88ad0e22e80ede15af
mvr <matthewvonrocketstein@gmail-dot-com>
2013-04-20 17:02:10 -0400

    sq

--- lib/ixle/schema.py
+++ lib/ixle/schema.py
@@ -1,5 +1,6 @@
 """ ixle.schema
 """
+import math
 from datetime import datetime
 from couchdb.mapping import Document
 from couchdb.mapping import TextField, IntegerField, DateTimeField, ListField
@@ -51,6 +52,11 @@ class Item(Document):
         except UnicodeEncodeError,e:
             print self.id
             raise
+
+    @property
+    def size_mb(self):
+        """ get approx size in megabytes """
+        return self.size and math.ceil(self.size*1.0/1024)
 
     @property
     def dirname(self):
