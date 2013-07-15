7100b2f02b64e4cc0fe1d9f1feb0cf4ff64dab3e
matt@laptop <matt@laptop>
2013-07-14 23:50:28 -0400

    abstract daemon_data_dir

--- lib/ixle/engine/_mongo.py
+++ lib/ixle/engine/_mongo.py
@@ -2,6 +2,9 @@
 """
 from .base import Engine
 class MongoDB(Engine):
+    def start_daemon(self):
+        from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
+
     def get_server(self):
         return MongoClient(self.settings['mongo']['server'],
                            self.settings['mongo']['port'])
