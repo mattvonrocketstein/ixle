b86a489b96365a5ca82753c2c48ffaa987aed51d
matt@laptop <matt@laptop>
2013-09-24 09:44:17 -0400

    add model for remote hosts

--- lib/ixle/views/search.py
+++ lib/ixle/views/search.py
@@ -6,8 +6,7 @@ from report import report
 #from ixle.query import javascript
 from ixle.schema import Item
 
-per_page = 15
-from flask_mongoengine.pagination import Pagination
+from ixle.pages import per_page, Pagination
 
 class Search(View):
 
@@ -25,28 +24,15 @@ class Search(View):
     def get_ctx(self):
         search_query = self['_']
         page = int(self['p'] or 1)
-        start = 0
-        #end = page_size
-
         items = Item.contains(search_query)
         pitems = Pagination(items, page, per_page)
-        items=pitems.items
+        items = pitems.items
         num_results = pitems.total
         num_pages = pitems.pages
-
-        #if self.ajax and search_query:
-            #couch_query = self.get_couch_query(search_query)
-            #start = page_size*(page-1)
-            #end   = page_size * page
-            #keys = (self.db%couch_query)[ start : end ]
-            #items = [ Item.load(self.db, k) for k in keys ]
-        #else:
-        #    items =
         return dict(_=search_query,
                     pagination=pitems, is_dir='',
                     num_results=num_results,
                     query=search_query, items=items,
-                    #start=start, end=end
                     )
 
     def main(self):
