a1f5958a08338494e42c14e8fee75a8e1fa6ef25
mvr <matthewvonrocketstein@gmail-dot-com>
2013-11-28 11:44:46 -0500

    handle tildas and empty val for path args

--- lib/ixle/views/browser.py
+++ lib/ixle/views/browser.py
@@ -1,10 +1,10 @@
-"""ixle.views.browser
+""" ixle.views.browser
 """
 from ixle.views.search import Search
 from corkscrew.views import BluePrint
 from report import report
-#from ixle.query import javascript
 from ixle.schema import Item
+from ixle.python import expanduser
 
 class Browser(Search):
 
@@ -18,18 +18,21 @@ class Browser(Search):
         from ixle.agents import registry
         ctx = super(Browser, self).get_ctx()
         qstring = self['_']
+        if not qstring:
+            qstring='~'
+        if qstring.startswith('~'):
+            return self.redirect(self.url+'?_='+expanduser(qstring))
         if ope(qstring): # should be a dir already..
             contents = [ [x, opj(qstring,x)] for x in os.listdir(qstring)]
-
         else:
             contents = []
         subddirs = filter(lambda x: os.path.isdir(x[1]), contents)
         files = dict([x for x in contents if os.path.isfile(x[1])])
-
         ctx.update(subddirs=subddirs,
                    is_dir='yes', files=files,
                    agent_types = registry.keys())
         return ctx
 
     def get_couch_query(self, search_query):
+        raise Exception, 'niy'
         return javascript.key_startswith(search_query)
