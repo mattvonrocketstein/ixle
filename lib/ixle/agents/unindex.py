cf585ce720efaa465a956b38e4432b5a3c79809d
matt@laptop <matt@laptop>
2013-11-28 11:45:35 -0500

    quiet down the output for unindexer

--- lib/ixle/agents/unindex.py
+++ lib/ixle/agents/unindex.py
@@ -8,8 +8,6 @@ class Unindex(DestructionMixin, ItemIterator):
     count = 1
 
     def callback(self, item=None, **kargs):
-        print item,kargs
         self.delete_item(item)
         if self.count==1:
             self.count+=1
-            from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
