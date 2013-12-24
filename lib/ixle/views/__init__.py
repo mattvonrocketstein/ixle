17051cc23793de7cb0fd0bc7fa19e2bcdc9eb98c
matt@laptop <matt@laptop>
2013-12-24 16:32:48 -0500

    simplifying comet demo

--- lib/ixle/views/__init__.py
+++ lib/ixle/views/__init__.py
@@ -140,16 +140,12 @@ Fext = generate_attribute_filter_view('fext', label='extensions')
 FileTypeView  = generate_attribute_filter_view('file_type', label='types')
 MovieView  = generate_attribute_filter_view('is_movie', label='is_movie')
 
-from goulash.stdout import ThreadedStdout
-
 import time
 import sys, threading
 from Queue import Queue, Empty
-from ansi2html import Ansi2HTMLConverter
 from goulash.stdout import ThreadedStdout
+from goulash.ansi import Ansi2HTMLConverter
 
-conv = Ansi2HTMLConverter()
-unansi = conv.convert
 fake = ThreadedStdout(sys.stdout)
 fake.install()
 
@@ -158,35 +154,58 @@ class Chat(SijaxView):
 
     url = '/chat'
     template = "chat.html"
+    extra_scripts=[ '/static/js/sijax/sijax.js',
+                    '/static/js/sijax/sijax_comet.js' ]
 
     def worker(self):
-        from ixle.api import indexer
-        indexer('/home/vagrant/code/ixle/')
+        #from ixle.api import indexer
+        #indexer('/home/vagrant/code/ixle/')
+        import time
+        self.report('stuff')
+        print 1; time.sleep(1)
+        print 2; time.sleep(2)
+        print 3; time.sleep(1)
+        print 4; time.sleep(2)
+        report("print 5; time.sleep(2)")
+        report("print 6; time.sleep(2)")
+        print 7;
+        print 4;
 
     def comet_handler(self, obj_response):
         """ """
+        conv = Ansi2HTMLConverter(inline=False, escaped=False)
+        unansi = conv.convert
+        obj_response.html_append('#comet_data', conv.get_style())
         thr = threading.Thread(target=self.worker, name='testing')
         q = fake.register(thr)
         thr.start()
-        nothing_written = 0
-        while thr.is_alive():
-            zult = fake.read_for(thr).strip()
+
+        hidebusy = lambda:obj_response.css('#loading_icon', 'display', 'none')
+        showbusy = lambda:obj_response.css('#loading_icon', 'display', 'block')
+
+        def read():
+            zult = fake.read_all(thr).replace('\n','<br/>')
             if zult:
-                obj_response.html_append('#comet_data', unansi(zult))
+                zult = unansi(zult)
+                obj_response.html_append('#comet_data', zult)
+                hidebusy()
             else:
-                nothing_written += 1
-                if nothing_written > 10:
-                    obj_response.html_append('#comet_data', '.')
-                    nothing_written = 0
+                showbusy()
+
+        while thr.is_alive():
+            read()
             yield obj_response
             time.sleep(.5)
+        read()
+        hidebusy()
+        yield obj_response
 
     def main(self):
         if self.is_sijax:
             self.sijax.register_comet_callback('do_work', self.comet_handler)
             out = self.sijax.process_request()
             return out
-        return self.render()
+        return self.render(javascript=self.sijax.get_js())
 
 
 __views__ = [
